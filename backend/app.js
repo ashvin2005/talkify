1cbdcb8709269d016bcf0c15060ccbd265c34180
Ashvin Tiwari <ashvintiwari161@gmail.com>
2026-02-07 08:04:48 +0530

    aap.js

--- backend/app.js
+++ backend/app.js
@@ -1,20 +1,31 @@
-import express from 'express';
-import { createServer } from 'http';
-import cors from 'cors';
-import { connectToSocket } from './src/controllers/socketManager.js';
-import userRoute from './src/routes/userRoute.js';
+import express from "express";
+import { createServer } from "node:http";
+import cors from "cors";
+
+import { connectToSocket } from "./src/controllers/socketManager.js";
+import userRoute from "./src/routes/userRoute.js";
+import { admin } from "./src/firebase.js";
 
 const app = express();
 const server = createServer(app);
 
+
 connectToSocket(server);
 
-app.use(cors());
-app.use(express.json());
-app.use('/api/v1/users', userRoute);
+app.set("port", process.env.PORT || 8000);
 
-const PORT = process.env.PORT || 8000;
+app.use(cors({
+  origin: "*",
+  methods: ["GET", "POST"],
+  allowedHeaders: ["*"],
+  credentials: true,
+}));
+app.use(express.json({ limit: "40kb" }));
+app.use(express.urlencoded({ limit: "40kb", extended: true }));
 
-server.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+
+app.use("/api/v1/users", userRoute);
+
+server.listen(app.get("port"), () => {
+  console.log(`Server listening on port ${app.get("port")}`);
 });
