cb4efc236d2014521c394360a85dc7f7b25301ab
Ashvin Tiwari <ashvintiwari161@gmail.com>
2026-02-25 15:58:00 +0530

    latest commit

--- backend/src/firebase.js
+++ backend/src/firebase.js
@@ -5,6 +5,7 @@ import admin from "firebase-admin";
 
 if (!admin.apps.length) {
   admin.initializeApp({
+    //
     credential: admin.credential.cert({
       projectId: process.env.FIREBASE_PROJECT_ID,
       clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
